3f6f00fcc066bd9dc966dc02f734231e129d025a
By-Kvn <kevin.labatte0@gmail.com>
2024-11-25 11:28:22 +0100

    feat(*): update the next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/portfolio-ci-cd',
+  assetPrefix: '/portfolio-ci-cd',
 };
 
 export default nextConfig;
